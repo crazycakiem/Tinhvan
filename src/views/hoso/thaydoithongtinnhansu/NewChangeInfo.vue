b4ed2e529d9846aea771cf8d7446bc847a41459c
lampt <lampt201092@gmail.com>
2018-12-11 14:50:10 +0700

    bo ts

--- src/views/hoso/thaydoithongtinnhansu/NewChangeInfo.vue
+++ src/views/hoso/thaydoithongtinnhansu/NewChangeInfo.vue
@@ -176,7 +176,7 @@
     </b-modal>
   </b-form>
 </template>
-<script lang="ts">
+<script>
 import Vue from "vue";
 import { Component, Prop } from "vue-property-decorator";
 import { bus } from "../../../main";
